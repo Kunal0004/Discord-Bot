e6056735e3db19c0215277e5b70fa4eb25556853
RigidStudios <34319439+RigidStudios@users.noreply.github.com>
2021-02-23 16:35:46 +0400

    8ball: code standard, minor fixes, optimizations

--- commands/8ball.js
+++ commands/8ball.js
@@ -1,18 +1,21 @@
-const Discord = require('discord.js');
-
 const questions = ["It is certain.", "It is decidedly so.", "Without a doubt.", "Yes - definitely.", "You may rely on it.", "As I see it, yes.", "Most likely.", "Outlook good.", "Yes.", "Signs point to yes.", "Reply hazy, try again.", "Ask again later.", "Better not tell you now.", "Cannot predict now.", "Concentrate and ask again.", "Don't count on it.", "My reply is no.", "My sources say no.", "Outlook not so good.", "Very doubtful."]
 
 module.exports = {
     name: '8ball',
-    aliases: ['luck, pick, chose'],
+    aliases: ['luck', 'pick', 'choose'],
     description: 'Awnsers your question',
 
     execute(client, prefix, message, args) {
         const embed = new Discord.MessageEmbed()
-            .setTitle('`🎱` 8Ball')
-            .addFields({name: `${message.author.username}'s Question:`, value: `${args.join(' ')}`}, {name: `Awnser:`, value: `${questions[Math.round(Math.random()*questions.length)]}`})
-            .setColor('#7CFC00')
-            .setFooter(`Requested By ${message.author.username}#${message.author.discriminator}`, message.author.displayAvatarURL())
+            .setTitle("`🎱` 8Ball")
+            .addFields({
+                name: `${message.author.username}'s Question:`,
+                value: args.join(' '), {
+                name: `Answer:`,
+                value: questions[Math.floor(Math.random()*questions.length)]
+            })
+            .setColor("#7CFC00")
+            .setFooter(`Requested By ${message.author.tag}`, message.author.displayAvatarURL())
             .setTimestamp()
         return message.channel.send(embed)
     }
